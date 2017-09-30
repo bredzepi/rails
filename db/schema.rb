44a42392b18fa8f3a879f1fc81967353e4da9d44
Beazit Redzepi <redzepi.beazit@gmail.com>
2017-09-30 15:57:00 +0000

    added price field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930145525) do
+ActiveRecord::Schema.define(version: 20170930154915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170930145525) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.integer "price"
   end
 
 end
