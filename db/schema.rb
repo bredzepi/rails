978deab7d441c58a30f29b8cb900a9b51489f2e5
Beazit Redzepi <redzepi.beazit@gmail.com>
2017-10-03 10:18:09 +0000

    created orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001212711) do
+ActiveRecord::Schema.define(version: 20171003090816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
