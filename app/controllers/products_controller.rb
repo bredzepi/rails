c692b59c2547ec22b237d6a49442dbeebf72afd4
Beazit Redzepi <redzepi.beazit@gmail.com>
2017-10-16 12:28:08 +0000

    5.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,18 +24,18 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+  @product = Product.new(product_params)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+  respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product, notice: 'Product was successfully created.' }
+      format.json { render :show, status: :created, location: @product }
+    else
+      format.html { render :new }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
     end
   end
+end
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
